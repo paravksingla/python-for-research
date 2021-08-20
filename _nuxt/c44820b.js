cf9e5c08d229ea31186dbbbe0cb596d37fef937e
paravksingla <88997563+paravksingla@users.noreply.github.com>
2021-08-20 15:37:57 +0530

    Add version 0.1
    Blank Template

---
+++ _nuxt/c44820b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{296:function(e,t,n){"use strict";n.r(t);var l=n(4),component=Object(l.a)({},(function(){var e=this,t=e.$createElement;return(e._self._c||t)("span",{staticClass:"text-ss align-middle ml-2 px-1 py-0 rounded-sm lowercase self-center"},[e._t("default")],2)}),[],!1,null,null,null);t.default=component.exports}}]);
